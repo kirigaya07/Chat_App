3b3649bab83d94066b06262a5bd790887a1ff3f5
kirigaya07 <crimsomemoon7@gmail.com>
2025-01-25 18:20:20 +0530

    Initial Setup

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.route.js";
+import { connnectDB } from "./lib/db.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(5001, () => {
+  console.log(`Server is running on port : ${PORT}`);
+  connnectDB();
+});
