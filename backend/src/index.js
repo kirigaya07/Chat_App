c28338ecb20ce7cce15c1c9dc93c515c8ada37b2
kirigaya07 <crimsomemoon7@gmail.com>
2025-01-29 14:31:25 +0530

    Message Route done

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
-import authRoutes from "./routes/auth.route.js";
 import { connnectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 const app = express();
@@ -13,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(5001, () => {
   console.log(`Server is running on port : ${PORT}`);
---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import {
+  getMessages,
+  getUsersForSiderbar,
+  sendMessage,
+} from "../controllers/message.controller.js";
+
+const router = express.Router();
+
+router.get("/users", protectRoute, getUsersForSiderbar);
+router.get("/:id", protectRoute, getMessages);
+
+router.post("/send/:id", protectRoute, sendMessage);
+
+export default router;
