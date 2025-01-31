e93ab53ca4d2e204541c3e81e19979d1a5cd4e41
kirigaya07 <crimsomemoon7@gmail.com>
2025-01-31 14:31:03 +0530

    Login page completed

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -21,6 +21,19 @@ export const useAuthStore = create((set) => ({
     }
   },
 
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data });
+      toast.success("Logged in successfully");
+    } catch (error) {
+      toast.error(error.response?.data?.message);
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+
   signup: async (data) => {
     set({ isSigningUp: true });
     try {
@@ -31,6 +44,16 @@ export const useAuthStore = create((set) => ({
       toast.error(error.response?.data?.message);
     } finally {
       set({ isSigningUp: false });
+    }
+  },
+
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+    } catch (error) {
+      toast.error(error.response?.data?.message);
     }
   },
 }));
