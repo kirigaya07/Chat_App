c0ccbb4b9dd431bc2e19d01f502a13514ea7f06f
kirigaya07 <crimsomemoon7@gmail.com>
2025-02-15 00:47:30 +0530

    Real time connection function completed

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -11,6 +11,10 @@ const io = new Server(server, {
   },
 });
 
+export function getReceiverSocketId(userId) {
+  return userSocketMap[userId];
+}
+
 // used to store onlien users
 const userSocketMap = {}; //{userId: socketId}
 
