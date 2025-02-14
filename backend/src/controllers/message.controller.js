46ab031d171e08a8f4ed56dee5dc4b90b7df49a6
kirigaya07 <crimsomemoon7@gmail.com>
2025-02-15 00:48:36 +0530

    Real Time Chat Implemented

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -2,6 +2,7 @@ import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
 import cloudinary from "../lib/cloudinary.js";
+import { getReceiverSocketId, io } from "../lib/socket.js";
 
 export const getUsersForSiderbar = async (req, res) => {
   try {
@@ -57,6 +58,11 @@ export const sendMessage = async (req, res) => {
     });
 
     await newMessage.save();
+
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
 
     res.status(201).json(newMessage);
   } catch (error) {
